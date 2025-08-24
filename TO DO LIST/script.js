bda6be11a1227be616497036be8be50cf80683e0
Krish patel <krishhpatel1905@gmail.com>
2025-08-24 14:48:41 +0530

    CheckBox Added

--- TO DO LIST/script.js
+++ TO DO LIST/script.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
   function add() {
     let task = document.getElementById("task");
     const taskinput = task.value.trim();
@@ -18,6 +20,19 @@
     removebtn.onclick = function () {
       li.remove();
     };
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+       li.prepend(checkbox);
+       
+   checkbox.onchange = function () {
+  if (checkbox.checked) {
+    li.style.textDecoration = "line-through";
+  } else {
+    li.style.textDecoration = "none";
+  }
+};
+
 
     li.appendChild(removebtn);
     document.getElementById("taskList").appendChild(li);
